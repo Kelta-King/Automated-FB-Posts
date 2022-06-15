a09b9cbdab8bcad35ff037df953fd048d1c432d1
Kelta-King <kushangshah131@gmail.com>
2022-06-15 18:36:24 +0530

    new images

--- Actions/facebook.py
+++ Actions/facebook.py
@@ -3,6 +3,7 @@ import requests
 def publishOnPage( pageId, pageAccessToken, image_url, caption ):
 
     request_url = "https://graph.facebook.com/"+ pageId +"/photos?url="+ image_url +"&message=" + caption + "&access_token="+ pageAccessToken
-    data = requests.post(url = request_url)
     print(request_url)
+    data = requests.post(url = request_url)
+    print(data)
     # return request_url
--- fb.py
+++ fb.py
@@ -3,14 +3,14 @@ from Includes import config as conf
 import json
 from Actions import facebook as fb
 
-start = 22
-end = 30
+start = 0
+end = 100
 
-x = requests.get(url = "https://picsum.photos/v2/list")
+x = requests.get(url = "https://picsum.photos/v2/list?page=3&limit=100")
 dt = json.loads(x.text)
 
 i = start
-while ( i <= end ):
+while ( i < end ):
     
     try:
         image_url = dt[i]['download_url']
